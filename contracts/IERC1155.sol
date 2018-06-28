e676819412f6d8ffe9d35de09498e9b8c85d54c3
Witek <witek@enjin.com>
2018-06-28 01:02:00 -0700

    ERC-1155 update interface

--- contracts/IERC1155.sol
+++ contracts/IERC1155.sol
@@ -2,15 +2,17 @@ pragma solidity ^0.4.24;
 
 interface IERC1155 {
     // Events
-    event Transfer(uint256 indexed _itemId, address indexed _from, address indexed _to, uint256 _value);
-    event Approval(uint256 indexed _itemId, address indexed _owner, address indexed _spender, uint256 _value);
+    event Approval(address indexed _owner, address indexed _spender, uint256 indexed _itemId, uint256 _value);
+    event Transfer(address indexed _from, address indexed _to, uint256 indexed _itemId, uint256 _value);
 
     // Required Functions
-    function transfer(uint256[] _itemId, address[] _to, uint256[] _value) external returns (bool success);
-    function transferFrom(uint256[] _itemId, address[] _from, address[] _to, uint256[] _value) external returns (bool success);
-    function approve(uint256[] _itemId, address[] _spender, uint256[] _value) external returns (bool success);
-    function increaseApproval(uint256[] _itemId, address[] _spender, uint256[] _addedValue) external returns (bool success);
-    function decreaseApproval(uint256[] _itemId, address[] _spender, uint256[] _subtractedValue) external returns (bool success);
+    function transfer(address _to, uint256 _itemId, uint256 _value) external;
+    function transferFrom(address _from, address _to, uint256 _itemId, uint256 _value) external;
+    function batchTransfer(address _to, uint256[] _itemIds, uint256[] _values) external;
+    function batchTransferFrom(address _from, address _to, uint256[] _itemIds, uint256[] _values) external;
+    function approve(address[] _spenders, uint256[] _itemIds, uint256[] _values) external;
+    function increaseApproval(address[] _spenders, uint256[] _itemIds, uint256[] _addedValues) external;
+    function decreaseApproval(address[] _spenders, uint256[] _itemIds, uint256[] _subtractedValues) external;
 
     // Required View Functions
     function totalSupply(uint256 _itemId) external view returns (uint256);
