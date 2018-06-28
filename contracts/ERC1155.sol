7badb78d1ac9c65601c631851a7664a7bfdbea50
Witek <witek@enjin.com>
2018-06-28 16:02:49 -0700

    ERC-1155 cleanup, wip nfi support

--- contracts/ERC1155.sol
+++ contracts/ERC1155.sol
@@ -16,47 +16,37 @@ contract ERC1155 {
     mapping (uint256 => string) public symbol;
     mapping (uint256 => mapping(address => mapping(address => uint256))) allowances;
     mapping (uint256 => Items) public items;
-    mapping (uint256 => address) nfiOwners;
     mapping (uint256 => string) metadataURIs;
 
+
     // Events
-    //event Transfer(address indexed _from, address indexed _to, uint256[] indexed _itemIds, uint256[] _values);
-    //event Approval(address indexed _owner, address indexed _spender, uint256[] indexed _itemIds, uint256[] _values);
     event Approval(address indexed _owner, address indexed _spender, uint256 indexed _itemId, uint256 _value);
     event Transfer(address indexed _from, address indexed _to, uint256 indexed _itemId, uint256 _value);
 
     // TEMP CONSTRUCTOR - Testing purposes
-    /**
     constructor() public {
         items[1].balances[msg.sender] = 1000;
         items[2].balances[msg.sender] = 1000;
         items[3].balances[msg.sender] = 1000;
-        items[4].balances[msg.sender] = 1000;
-        items[5].balances[msg.sender] = 1000;
-        items[6].balances[msg.sender] = 1000;
-        items[7].balances[msg.sender] = 1000;
-        items[8].balances[msg.sender] = 1000;
-        items[9].balances[msg.sender] = 1000;
-        items[10].balances[msg.sender] = 1000;
-        items[11].balances[msg.sender] = 1000;
-        items[12].balances[msg.sender] = 1000;
-        items[13].balances[msg.sender] = 1000;
-        items[14].balances[msg.sender] = 1000;
-        items[15].balances[msg.sender] = 1000;
-        items[16].balances[msg.sender] = 1000;
-        items[17].balances[msg.sender] = 1000;
-        items[18].balances[msg.sender] = 1000;
-        items[19].balances[msg.sender] = 1000;
-        items[20].balances[msg.sender] = 1000;
     }
-    */
 
     // Required Functions
+    function transfer(address _to, uint256 _itemId, uint256 _value) external {
+        require(_value <= items[_itemId].balances[msg.sender]);
+
+        items[_itemId].balances[msg.sender] = items[_itemId].balances[msg.sender].sub(_value);
+        items[_itemId].balances[_to] = _value.add(items[_itemId].balances[_to]);
+        Transfer(msg.sender, _to, _itemId, _value);
+    }
+
     function transferFrom(address _from, address _to, uint256 _itemId, uint256 _value) external {
+        require(_value <= items[_itemId].balances[_from]);
+
         if(_from != msg.sender) {
             require(allowances[_itemId][_from][msg.sender] >= _value);
             allowances[_itemId][_from][msg.sender] = allowances[_itemId][_from][msg.sender].sub(_value);
         }
+
         items[_itemId].balances[_from] = items[_itemId].balances[_from].sub(_value);
         items[_itemId].balances[_to] = _value.add(items[_itemId].balances[_to]);
 
@@ -64,18 +54,25 @@ contract ERC1155 {
     }
 
     function batchTransfer(address _to, uint256[] _itemIds, uint256[] _values) external {
+        require(_value <= items[_itemId].balances[msg.sender]);
+
         uint256 _itemId;
         uint256 _value;
+
         for (uint256 i = 0; i < _itemIds.length; ++i) {
             _itemId = _itemIds[i];
             _value = _values[i];
+
             items[_itemId].balances[msg.sender] = items[_itemId].balances[msg.sender].sub(_value);
             items[_itemId].balances[_to] = _value.add(items[_itemId].balances[_to]);
+
             Transfer(msg.sender, _to, _itemId, _value);
         }
     }
 
     function batchTransferFrom(address _from, address _to, uint256[] _itemIds, uint256[] _values) external {
+        require(_value <= items[_itemId].balances[_from]);
+
         uint256 _itemId;
         uint256 _value;
 
@@ -83,8 +80,10 @@ contract ERC1155 {
             for (uint256 i = 0; i < _itemIds.length; ++i) {
                 _itemId = _itemIds[i];
                 _value = _values[i];
+
                 items[_itemId].balances[_from] = items[_itemId].balances[_from].sub(_value);
                 items[_itemId].balances[_to] = _value.add(items[_itemId].balances[_to]);
+
                 Transfer(_from, _to, _itemId, _value);
             }
         }
@@ -166,17 +165,7 @@ contract ERC1155 {
         return items[_itemId].name;
     }
 
-    // Optional Functions for Non-Fungible Items
-    function ownerOf(uint256 _itemId) external view returns (address) {
-        return nfiOwners[_itemId];
-    }
     function itemURI(uint256 _itemId) external view returns (string) {
         return metadataURIs[_itemId];
-    }
-    function itemByIndex(uint256 _itemId, uint256 _index) external view returns (uint256) {
-
-    }
-    function itemOfOwnerByIndex(uint256 _itemId, address _owner, uint256 _index) external view returns (uint256) {
-
     }
 }
---
+++ contracts/ERC1155NonFungible.sol
@@ -0,0 +1,57 @@
+pragma solidity ^0.4.0;
+
+import "./ERC1155.sol";
+
+/**
+    @dev Extension to ERC1155 for Non-Fungible Items support
+    Work-in-progress
+*/
+contract ERC1155NonFungible is ERC1155 {
+    uint256 constant ID_MASK = uint256(uint128(~0)) << 128;
+    uint256 constant NONFUNGIBLE_IDX_MASK = uint128(~0);
+
+    mapping (uint256 => bool) isNonFungible;
+    mapping (uint256 => address) nfiOwners;
+
+    function ownerOf(uint256 _itemId) external view returns (address) {
+        return nfiOwners[_itemId];
+    }
+
+    function itemByIndex(uint256 _itemId, uint256 _index) external view returns (uint256) {
+        // can't call this on a non-fungible item directly, only its underlying itemId
+        require(_itemId > 0 && _itemId & NONFUNGIBLE_IDX_MASK == 0, "base");
+
+        uint256 nfiId = _itemId.add(_index);
+
+        // must be a valid ERC721 itemId
+        require(LibItemCommon.isValidNonFungibleItemId(data, nfiId), "erc721");
+
+        return nfiId;
+    }
+    function itemOfOwnerByIndex(uint256 _itemId, address _owner, uint256 _index) external view returns (uint256) {
+        // can't call this on a non-fungible item directly, only its underlying itemId
+        require(_itemId > 0 && _itemId & NONFUNGIBLE_IDX_MASK == 0, "base");
+        require(isNonFungible[_itemId & ID_MASK]), "non-fungible");
+        require(_index < balanceOf(data, _itemId, _owner), "balance");
+
+        uint256 numToSkip = _index;
+        uint256 maxIndex = items[_itemId].totalSupply;
+
+        // rather than spending gas storing all this, loop the supply and find the item
+        for (uint i = 1; i <= maxIndex; ++i) {
+
+            uint256 nfiId = _itemId.add(i);
+            address nfiOwner = nfiOwners(nfiId);
+
+            if (nfiOwner == _owner) {
+                if (numToSkip == 0) {
+                    return nfiId;
+                } else {
+                    numToSkip = numToSkip.sub(1);
+                }
+            }
+        }
+
+        return 0;
+    }
+}
