0b8219d04c9faa6713983f6b120627fb5f6efe55
Alan Lu <alanlu1023@gmail.com>
2018-11-14 14:32:50 -0600

    Compiler enforces IERC1155 is IERC165

--- contracts/IERC1155.sol
+++ contracts/IERC1155.sol
@@ -7,7 +7,7 @@ import "openzeppelin-solidity/contracts/introspection/IERC165.sol";
     @dev See https://github.com/ethereum/EIPs/blob/master/EIPS/eip-1155.md
     Note: The ERC-165 identifier for this interface is 0xd9b67a26.
  */
-interface IERC1155 /*is IERC165*/ {
+/*interface*/ contract IERC1155 is IERC165 {
     /**
         @dev Either TransferSingle or TransferBatch MUST emit when tokens are transferred, including zero value transfers as well as minting or burning.
         Either event from address `0x0` signifies a minting operation.
