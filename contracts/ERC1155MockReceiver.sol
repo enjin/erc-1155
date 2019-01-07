147e9bad455bbf5c8b73d3436f7ee1d827ea05f4
James Therien <james@enjin.com>
2019-01-07 09:24:42 -0500

    MockReceiver now returns the right values for batch receive.

--- contracts/ERC1155MockReceiver.sol
+++ contracts/ERC1155MockReceiver.sol
@@ -3,13 +3,15 @@ pragma solidity ^0.5.0;
 // Contract to test safe transfer behavior.
 contract ERC1155MockReceiver {
     bytes4 constant public ERC1155_RECEIVED = 0xf23a6e61;
-    bytes4 constant public NOT_ERC1155_RECEIVED = 0xa23a6e60;
+    bytes4 constant public ERC1155_BATCH_RECEIVED = 0xbc197c81;
+    bytes4 constant public NOT_ERC1155_RECEIVED = 0xa23a6e60; // Some random value
 
     // Keep values from last received contract.
     bool public shouldReject;
 
     bytes public lastData;
     address public lastOperator;
+    address public lastFrom;
     uint256 public lastId;
     uint256 public lastValue;
 
@@ -17,8 +19,9 @@ contract ERC1155MockReceiver {
         shouldReject = _value;
     }
 
-    function onERC1155Received(address _operator, address /*_from*/, uint256 _id, uint256 _value, bytes calldata _data) external returns(bytes4) {
+    function onERC1155Received(address _operator, address _from, uint256 _id, uint256 _value, bytes calldata _data) external returns(bytes4) {
         lastOperator = _operator;
+        lastFrom = _from;
         lastId = _id;
         lastValue = _value;
         lastData = _data;
@@ -29,15 +32,16 @@ contract ERC1155MockReceiver {
         }
     }
 
-    function onERC1155BatchReceived(address _operator, address /*_from*/, uint256[] calldata _ids, uint256[] calldata _values, bytes calldata _data) external returns(bytes4) {
+    function onERC1155BatchReceived(address _operator, address _from, uint256[] calldata _ids, uint256[] calldata _values, bytes calldata _data) external returns(bytes4) {
         lastOperator = _operator;
+        lastFrom = _from;
         lastId = _ids[0];
         lastValue = _values[0];
         lastData = _data;
         if (shouldReject == true) {
             return NOT_ERC1155_RECEIVED;
         } else {
-            return ERC1155_RECEIVED;
+            return ERC1155_BATCH_RECEIVED;
         }
     }
 }
