7edfaa9ec537544ca1fdb0b93cbb96bbef5455fd
James Therien <james@enjin.com>
2019-01-07 08:20:56 -0500

    Fix package and expectThrow. Tests work from a fresh build.

--- test/ERC1155Mintable.js
+++ test/ERC1155Mintable.js
@@ -1,7 +1,7 @@
 /* global artifacts, contract, it, assert */
 /* eslint-disable prefer-reflect */
 
-import expectThrow from './helpers/expectThrow';
+const expectThrow = require('./helpers/expectThrow');
 
 const ERC1155Mintable = artifacts.require('ERC1155Mintable.sol');
 const ERC1155MockReceiver = artifacts.require('ERC1155MockReceiver.sol');
--- test/helpers/expectThrow.js
+++ test/helpers/expectThrow.js
@@ -1,4 +1,4 @@
-export default async promise => {
+module.exports = async promise => {
   try {
     await promise;
   } catch (error) {
